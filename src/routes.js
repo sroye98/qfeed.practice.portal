158adb05b660dd352909e9220381ed75c29b0d34
Saachi Roye <saachi.roye@gmail.com>
2021-07-16 17:45:06 -0500

    added login pages and updated routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 
 import Dashboard from './views/Dashboard';
+import Login from './views/Login';
 import Patients from './views/Patients';
 import Queues from './views/Queues';
 import Recordings from './views/Recordings';
@@ -9,7 +10,8 @@ const Routes = (props) => {
     return (
         <div className="site-layout-content">
             <Switch>
-                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/" component={Login} />
+                <Route exact path="/dashboard" component={Dashboard} />
                 <Route exact path="/patients" component={Patients} />
                 <Route exact path="/queues" component={Queues} />
                 <Route exact path="/recordings" component={Recordings} />
---
+++ src/views/Login.js
@@ -0,0 +1,29 @@
+import { useHistory } from 'react-router-dom';
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const Login = (props) => {
+    const handleLogin = () => {
+        useHistory("/dashboard");
+    }
+
+    return (
+        <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }} onFinish={handleLogin}>
+            <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+                <Input />
+            </Form.Item>
+            <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+                <Input.Password />
+            </Form.Item>
+            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default Login;
