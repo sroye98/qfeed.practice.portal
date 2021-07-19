9f647c064989a58d36b28bba10d04249bb19efa9
Saachi Roye <saachi.roye@gmail.com>
2021-07-19 10:26:12 -0500

    Update Login.js

--- src/views/Login.js
+++ src/views/Login.js
@@ -5,22 +5,22 @@ const Login = (props) => {
     const history = useHistory();
 
     const handleLogin = () => {
-        history("/dashboard");
+        history.push("/dashboard");
     };
 
     return (
         <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }} onFinish={handleLogin}>
-            <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+            <Form.Item label="Username" name="username" rules={[{ required: false, message: 'Please input your username!' }]}>
                 <Input />
             </Form.Item>
-            <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+            <Form.Item label="Password" name="password" rules={[{ required: false, message: 'Please input your password!' }]}>
                 <Input.Password />
             </Form.Item>
             <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
                 <Checkbox>Remember me</Checkbox>
             </Form.Item>
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
+                <Button type="primary" htmlType="submit" onClick={handleLogin}>
                     Submit
                 </Button>
             </Form.Item>
